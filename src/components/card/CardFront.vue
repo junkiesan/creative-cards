97922b80d3aafd08e22f7cfd0329f2a15cc7a909
Julien Bouland <bouland.julien@gmail.com>
2020-12-24 17:04:09 +0100

    text output component

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -4,18 +4,18 @@
       <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
       <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
       <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
-      {{textBoxValue1}}
-      {{textBoxValue2}}
-      {{textBoxValue3}}
     </div>
     <div class="col-sm-6 card card-display">
-
+      <cc-text-output></cc-text-output>
+      <cc-text-output></cc-text-output>
+      <cc-text-output></cc-text-output>
     </div>
   </div>
 </template>
 
 <script>
 import TextInput from './TextInput.vue'
+import TextOutput from './TextOutput.vue'
 
 export default {
   data: function () {
@@ -26,7 +26,9 @@ export default {
     }
   },
   components: {
-    ccTextInput: TextInput
+    ccTextInput: TextInput,
+    ccTextOutput: TextOutput
+
   }
 }
 </script>
