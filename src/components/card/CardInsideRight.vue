678528c6c981b1d6e821776184110d8f2a4c3746
Julien Bouland <bouland.julien@gmail.com>
2020-12-28 11:39:18 +0100

    style removed for other cards

--- src/components/card/CardInsideRight.vue
+++ src/components/card/CardInsideRight.vue
@@ -16,14 +16,5 @@ export default {
 </script>
 
 <style>
-  .edit-area {
-    background: #d2f9f9;
-    padding: 20px;
-    height: 800px;
-  }
 
-  .card-display {
-    height: 800px;
-    padding: 20px;
-  }
 </style>
